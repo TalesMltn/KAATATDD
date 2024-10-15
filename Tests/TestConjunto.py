b95ac0491debe7b5563e563038ea3e83a2fe0d11
origin <72638273@continental.edu.pe>
2024-10-15 16:30:33 -0500

    Caso 3:Calcular promedio con conjunto de un elemento (Rojoo)

--- Tests/TestConjunto.py
+++ Tests/TestConjunto.py
@@ -7,9 +7,6 @@ class TestConjunto( unittest.TestCase):
     def test_conjunto_vacio_reptornaNone(self):
         conjunto = Conjunto ([5])
         self.asserEqual
-def test_conjunto_dosElementos_retornaPromedioElementos( self ):
-conjunto=Conjunto([5,7])
-self.assertEqual(6,conjunto.promedio())
-def test_conjunto_dosElementos_retornaPromedioElementos( self ):
-conjunto=Conjunto([5,7])
-self.assertEqual(6,conjunto.promedio())
+    def test_conjunto_dosElementos_retornaPromedioElementos( self ):
+        conjunto=Conjunto([5,7])
+        self.assertEqual(6,conjunto.promedio())
